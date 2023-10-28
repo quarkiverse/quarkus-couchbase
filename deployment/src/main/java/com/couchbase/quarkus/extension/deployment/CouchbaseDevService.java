19192a3660b950cd88ebb905fa6122e1c6078ded
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-10-28 19:51:07 +0200

    chore: fix formatting

--- deployment/src/main/java/com/couchbase/quarkus/extension/deployment/CouchbaseDevService.java
+++ deployment/src/main/java/com/couchbase/quarkus/extension/deployment/CouchbaseDevService.java
@@ -1,6 +1,5 @@
 package com.couchbase.quarkus.extension.deployment;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -23,77 +22,74 @@ import io.quarkus.deployment.dev.devservices.GlobalDevServicesConfig;
 
 public class CouchbaseDevService {
 
+    static volatile List<RunningDevService> devServices = new ArrayList<>();
 
-  static volatile List<RunningDevService> devServices = new ArrayList<>();
+    @BuildStep
+    List<DevServicesResultBuildItem> startCouchBase(
+            CuratedApplicationShutdownBuildItem closeBuildItem) {
+        QuarkusCouchbaseContainer couchbase = startContainer();
+        devServices.add(new RunningDevService(CouchbaseQuarkusExtensionProcessor.FEATURE,
+                couchbase.getContainerId(), couchbase::close, Map.of()));
+        closeBuildItem.addCloseTask(couchbase::close, true);
+        return devServices.stream().map(RunningDevService::toBuildItem).collect(Collectors.toList());
 
-  @BuildStep
-  List<DevServicesResultBuildItem> startCouchBase(
-      CuratedApplicationShutdownBuildItem closeBuildItem) {
-    QuarkusCouchbaseContainer couchbase = startContainer();
-    devServices.add(new RunningDevService(CouchbaseQuarkusExtensionProcessor.FEATURE,
-        couchbase.getContainerId(), couchbase::close, Map.of()));
-    closeBuildItem.addCloseTask(couchbase::close, true);
-    return devServices.stream().map(RunningDevService::toBuildItem).collect(Collectors.toList());
-
-  }
-
-  private QuarkusCouchbaseContainer startContainer() {
-    String userName = ConfigProvider.getConfig()
-        .getOptionalValue("quarkus.couchbase.username", String.class).orElse("Administrator");;
-    String password = ConfigProvider.getConfig()
-        .getOptionalValue("quarkus.couchbase.password", String.class).orElse("password");
-    String version = ConfigProvider.getConfig().getValue("quarkus.couchbase.version", String.class);
-    QuarkusCouchbaseContainer couchbase =
-        new QuarkusCouchbaseContainer(version, userName, password);
-    couchbase.start();
-    return couchbase;
-  }
-
-  /**
-   * A {@link CouchbaseContainer} that exposes all ports to fixed values.
-   * <p>
-   * This is needed because the default {@link CouchbaseContainer} exposes ports to random values. 
-   */
-  private static class QuarkusCouchbaseContainer extends CouchbaseContainer {
-
-    // copied from org.testcontainers.couchbase.CouchbaseContainer
-    private static final int MGMT_PORT = 8091;
-    private static final int MGMT_SSL_PORT = 18091;
-    private static final int VIEW_PORT = 8092;
-    private static final int VIEW_SSL_PORT = 18092;
-    private static final int QUERY_PORT = 8093;
-    private static final int QUERY_SSL_PORT = 18093;
-    private static final int SEARCH_PORT = 8094;
-    private static final int SEARCH_SSL_PORT = 18094;
-    private static final int ANALYTICS_PORT = 8095;
-    private static final int ANALYTICS_SSL_PORT = 18095;
-    private static final int EVENTING_PORT = 8096;
-    private static final int EVENTING_SSL_PORT = 18096;
-    private static final int KV_PORT = 11210;
-    private static final int KV_SSL_PORT = 11207;
-
-
-    public QuarkusCouchbaseContainer(String version, String userName, String password) {
-      super("couchbase/server:" + version);
-      withCredentials(userName, password);
-      // we enable all non-enterprise services because we don't know which ones are needed
-      withEnabledServices(CouchbaseService.EVENTING, CouchbaseService.INDEX, CouchbaseService.KV,
-          CouchbaseService.QUERY, CouchbaseService.SEARCH);
-      addFixedExposedPort(MGMT_PORT, MGMT_PORT);
-      addFixedExposedPort(MGMT_SSL_PORT, MGMT_SSL_PORT);
-      addFixedExposedPort(ANALYTICS_PORT, ANALYTICS_PORT);
-      addFixedExposedPort(VIEW_PORT, VIEW_PORT);
-      addFixedExposedPort(VIEW_SSL_PORT, VIEW_SSL_PORT);
-      addFixedExposedPort(ANALYTICS_PORT, ANALYTICS_PORT);
-      addFixedExposedPort(ANALYTICS_SSL_PORT, ANALYTICS_SSL_PORT);
-      addFixedExposedPort(QUERY_PORT, QUERY_PORT);
-      addFixedExposedPort(QUERY_SSL_PORT, QUERY_SSL_PORT);
-      addFixedExposedPort(SEARCH_PORT, SEARCH_PORT);
-      addFixedExposedPort(SEARCH_SSL_PORT, SEARCH_SSL_PORT);
-      addFixedExposedPort(EVENTING_PORT, EVENTING_PORT);
-      addFixedExposedPort(EVENTING_SSL_PORT, EVENTING_SSL_PORT);
-      addFixedExposedPort(KV_PORT, KV_PORT);
-      addFixedExposedPort(KV_SSL_PORT, KV_SSL_PORT);
     }
-  }
+
+    private QuarkusCouchbaseContainer startContainer() {
+        String userName = ConfigProvider.getConfig()
+                .getOptionalValue("quarkus.couchbase.username", String.class).orElse("Administrator");
+        String password = ConfigProvider.getConfig()
+                .getOptionalValue("quarkus.couchbase.password", String.class).orElse("password");
+        String version = ConfigProvider.getConfig().getValue("quarkus.couchbase.version", String.class);
+        QuarkusCouchbaseContainer couchbase = new QuarkusCouchbaseContainer(version, userName, password);
+        couchbase.start();
+        return couchbase;
+    }
+
+    /**
+     * A {@link CouchbaseContainer} that exposes all ports to fixed values.
+     * <p>
+     * This is needed because the default {@link CouchbaseContainer} exposes ports to random values.
+     */
+    private static class QuarkusCouchbaseContainer extends CouchbaseContainer {
+
+        // copied from org.testcontainers.couchbase.CouchbaseContainer
+        private static final int MGMT_PORT = 8091;
+        private static final int MGMT_SSL_PORT = 18091;
+        private static final int VIEW_PORT = 8092;
+        private static final int VIEW_SSL_PORT = 18092;
+        private static final int QUERY_PORT = 8093;
+        private static final int QUERY_SSL_PORT = 18093;
+        private static final int SEARCH_PORT = 8094;
+        private static final int SEARCH_SSL_PORT = 18094;
+        private static final int ANALYTICS_PORT = 8095;
+        private static final int ANALYTICS_SSL_PORT = 18095;
+        private static final int EVENTING_PORT = 8096;
+        private static final int EVENTING_SSL_PORT = 18096;
+        private static final int KV_PORT = 11210;
+        private static final int KV_SSL_PORT = 11207;
+
+        public QuarkusCouchbaseContainer(String version, String userName, String password) {
+            super("couchbase/server:" + version);
+            withCredentials(userName, password);
+            // we enable all non-enterprise services because we don't know which ones are needed
+            withEnabledServices(CouchbaseService.EVENTING, CouchbaseService.INDEX, CouchbaseService.KV,
+                    CouchbaseService.QUERY, CouchbaseService.SEARCH);
+            addFixedExposedPort(MGMT_PORT, MGMT_PORT);
+            addFixedExposedPort(MGMT_SSL_PORT, MGMT_SSL_PORT);
+            addFixedExposedPort(ANALYTICS_PORT, ANALYTICS_PORT);
+            addFixedExposedPort(VIEW_PORT, VIEW_PORT);
+            addFixedExposedPort(VIEW_SSL_PORT, VIEW_SSL_PORT);
+            addFixedExposedPort(ANALYTICS_PORT, ANALYTICS_PORT);
+            addFixedExposedPort(ANALYTICS_SSL_PORT, ANALYTICS_SSL_PORT);
+            addFixedExposedPort(QUERY_PORT, QUERY_PORT);
+            addFixedExposedPort(QUERY_SSL_PORT, QUERY_SSL_PORT);
+            addFixedExposedPort(SEARCH_PORT, SEARCH_PORT);
+            addFixedExposedPort(SEARCH_SSL_PORT, SEARCH_SSL_PORT);
+            addFixedExposedPort(EVENTING_PORT, EVENTING_PORT);
+            addFixedExposedPort(EVENTING_SSL_PORT, EVENTING_SSL_PORT);
+            addFixedExposedPort(KV_PORT, KV_PORT);
+            addFixedExposedPort(KV_SSL_PORT, KV_SSL_PORT);
+        }
+    }
 }
