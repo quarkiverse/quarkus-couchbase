f592d42a5500a42c023268e6fdf4d2ef4b662b0f
Emilien Bevierre <emilien.bevierre@couchbase.com>
2024-10-09 16:54:53 +0100

    Upgrade to Quarkus 3.15.1 and SDK 3.7.3
    ----------
    Upgrade the extension to the latest Quarkus LTS and latest Java SDK release.
    
    -------
    - Bump Quarkus to 3.15.1
    - Bump Java SDK to 3.7.3
    - Update netty substitutions. As the official netty extension targets Netty 4.1.111.Final and our SDK uses 4.1.112.Final, some substitutions have been added to the "delete" step of the shell script.
    Specifically, this method signature has changed between the two versions (4.1.112 has 1 more string endpointIdentificationAlgorithm in the arguments) https://github.com/netty/netty/blob/ebe2aa5b7cd36562a20b024d78ecff47a86874b8/handler/src/main/java/io/netty/handler/ssl/JdkSslClientContext.java#L267-L273
    - The legacy @ConfigRoot approach for CouchbaseConfig was changed to the new @ConfigMapping one.
    - Added the compiler arg "-AlegacyConfigRoot=true" to the deployment module pom.xml, as the netty substitution still uses it.
    - Updated the way CouchbaseDevService gets the config fields.

--- deleteMethod.sh
+++ deleteMethod.sh
@@ -3,18 +3,34 @@ count_braces() {
     local file="$2"
 
     perl -ne '
-        BEGIN { $count = 0; $found = 0; $start_line = 0; }
-        if (/\b'"$method"'\s*\(/) { $found = 1; $start_line = $.; }
+        BEGIN { $count = 0; $found = 0; $in_method = 0; $start_line = 0; }
+
+        # Find the method or class name and ignore leading spaces
+        if (!$in_method && /^\s*\b'"$method"'\s*(\(\s*\))?/) {
+            $in_method = 1;
+            $start_line = $.;
+        }
+
+        # Go through each line until we find an opening brace (for multi-line method arguments)
+        if ($in_method) {
+            if (/.*\{/) {
+                $found = 1;
+                $in_method = 0;
+            }
+        }
+
+        # Once inside the body, count braces to support nested ones and print start and end when reaching the last
         if ($found) {
             $count += tr/{/{/;
             $count -= tr/}/}/;
-            if ($count == 0 && $found == 1) {
+            if ($count == 0) {
                 print "START:$start_line | END:$.\n";
                 exit;
             }
         }
     ' "$file"
 }
+
 
 delete_method() {
     local method="$1"
@@ -29,11 +45,11 @@ delete_method() {
     end_line=$(echo "$line_info" | cut -d':' -f3)
 
     if [ -n "$start_line" ] && [ -n "$end_line" ]; then
-        start_line=$((start_line - 1))
+        start_line=$((start_line - 1)) # start_line -1 to delete the annotation that comes before
         sed -i '' "${start_line},${end_line}d" "$file"
-        echo "Deleted method '$method' from $file (Lines: $start_line - $end_line)"
+        echo "Deleted method/class '$method' from $file (Lines: $start_line - $end_line)"
     else
-        echo "Method '$method' not found in $file"
+        echo "Method/Class '$method' not found in $file"
     fi
 }
 
