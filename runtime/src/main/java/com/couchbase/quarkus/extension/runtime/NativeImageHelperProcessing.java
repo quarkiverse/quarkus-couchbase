3c8a654dd46f16589bf38c1747e6be913d195257
Emilien Bevierre <emilien.bevierre@couchbase.com>
2024-11-14 11:22:07 +0000

    Adjusted MpscArrayQueue processing with future SDK changes

---
+++ runtime/src/main/java/com/couchbase/quarkus/extension/runtime/NativeImageHelperProcessing.java
@@ -0,0 +1,20 @@
+package com.couchbase.quarkus.extension.runtime;
+
+import java.lang.annotation.Native;
+import java.util.Queue;
+
+import com.couchbase.client.core.deps.org.jctools.queues.atomic.unpadded.MpscAtomicUnpaddedArrayQueue;
+import com.couchbase.client.core.util.NativeImageHelper;
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+
+public class NativeImageHelperProcessing {
+}
+
+@TargetClass(value = NativeImageHelper.class)
+final class Target_NativeImageHelper {
+    @Substitute
+    public static <T> Queue<T> createSizedQueue(int capacity) {
+        return new MpscAtomicUnpaddedArrayQueue<>(capacity);
+    }
+}
