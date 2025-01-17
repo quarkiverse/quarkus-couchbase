bfee380850ee1ad2e5699f5e5994e13c4d9de2a9
Emilien Bevierre <emilien.bevierre@couchbase.com>
2025-01-17 16:39:15 +0000

    Update docs on configuring metrics, add license header.

--- runtime/src/main/java/com/couchbase/quarkus/extension/runtime/CouchbaseRecorder.java
+++ runtime/src/main/java/com/couchbase/quarkus/extension/runtime/CouchbaseRecorder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2025 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.couchbase.quarkus.extension.runtime;
 
 import java.time.Duration;
@@ -8,10 +23,7 @@ import com.couchbase.client.java.ClusterOptions;
 import com.couchbase.client.java.env.ClusterEnvironment;
 import com.couchbase.client.metrics.micrometer.MicrometerMeter;
 
-import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.Metrics;
-import io.micrometer.core.instrument.config.MeterFilter;
-import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import io.quarkus.runtime.annotations.Recorder;
 
 @Recorder
